96801f21792abffed99c0ddf90787b2947366906
LucasP <lucaspinto.andrade@gmail.com>
2021-11-20 23:48:19 -0300

    products added

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import productController from '../controllers/product.controller.js';
 
 const router = express.Router();
+
+router.post("/", productController.createProduct);
+
+router.get("/", productController.getProducts);
+
+router.get("/:id", productController.getProduct)
+
+router.delete("/:id", productController.deleteProduct)
+
+router.put("/", productController.updateProduct);
 
 export default router;
